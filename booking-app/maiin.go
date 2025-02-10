ba68c3701044ce55843238e75e1653b5f91c66d2
Nicholas-Jason Roache <njrr325@gmail.com>
2025-02-10 18:33:48 +0000

    modified:   booking-app/maiin.go

--- booking-app/maiin.go
+++ booking-app/maiin.go
@@ -7,8 +7,9 @@ import (
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-
+	var remainingTickets = 30
 	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have ", conferenceTickets, " tickets and there are ", remainingTickets, " left")
 	fmt.Println("Get your tickets here to attend")
 
 }
