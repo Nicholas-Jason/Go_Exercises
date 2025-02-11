8b9ff4dde341b203088e8b58397c2a6073bf2271
Nicholas-Jason Roache <njrr325@gmail.com>
2025-02-11 19:41:15 +0000

    modified maiin

--- booking-app/maiin.go
+++ booking-app/maiin.go
@@ -1,15 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 30
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have ", conferenceTickets, " tickets and there are ", remainingTickets, " left")
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have %v tickets and there are %v left\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
