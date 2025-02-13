965209a31594c2c8eee0c25795b4866a91439cca
Nicholas-Jason Roache <njrr325@gmail.com>
2025-02-13 17:11:36 +0000

    added user input and slice

--- booking-app/maiin.go
+++ booking-app/maiin.go
@@ -11,12 +11,30 @@ func main() {
 	fmt.Printf("We have %v tickets and there are %v left\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 	// ask user for name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
 
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	var bookings = []string{}
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Theese are all the bookings %v\n", bookings)
+
+	fmt.Printf("Thank you User %v %v for booking %v tickets. We shall send a confirmation email to the address %v \n", firstName, lastName, userTickets, email)
+
+	fmt.Printf("There are %v remaining tickets for %v \n", remainingTickets, conferenceName)
 }
