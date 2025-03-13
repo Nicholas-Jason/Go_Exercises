3f17e2c6522e1e17fcab7ed8aa3c801cc6195b0a
Nicholas-Jason Roache <njrr325@gmail.com>
2025-03-13 13:36:51 +0000

    added tasks

--- TodoApp/main.go
+++ TodoApp/main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+var taskOne = "Practice Go"
+var taskTwo = "Workout"
+var taskThree = "Referesh my skills with Node JS"
+var taskFour = "Take a Break"
+var taskFive = "Go job hunting"
+
 func main() {
-	fmt.Print("Welcome to our TodoList App")
+	fmt.Println("###### Welcome to our TodoList App! ######")
+
+	fmt.Println("List of my Todos")
+
+	fmt.Println(taskOne)
+
+	fmt.Println(taskTwo)
+
+	fmt.Println(taskThree)
+
+	fmt.Println(taskFour)
+
+	fmt.Println(taskFive)
+
 }
