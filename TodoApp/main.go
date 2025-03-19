54d88e19394c7b7b9e45814e6fff330af3a8c154
Nicholas-Jason Roache <njrr325@gmail.com>
2025-03-19 13:48:29 +0000

    removed unused functions

--- TodoApp/main.go
+++ TodoApp/main.go
@@ -15,25 +15,15 @@ var taskItems = []string{taskOne, taskTwo, taskThree, taskFour, taskFive}
 
 //add more functions
 
-func showTask(taskItems []string) {
-	fmt.Println("List of my Todos")
-
-	for index, task := range taskItems {
-		fmt.Println(index+1, ":", task)
-	}
-}
-
-func addTask(taskItems []string, newTask string) []string {
-	var updatedTasks = append(taskItems, newTask)
-	return updatedTasks
-}
 func helloUser(writer http.ResponseWriter, request *http.Request) {
 	var greeting = "Hello user. Welcome to our Todolist App!"
 	fmt.Fprintf(writer, greeting)
 }
 
 func showTasks(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(writer, taskItems)
+	for _, task := range taskItems {
+		fmt.Fprintln(writer, task)
+	}
 }
 func main() {
 	fmt.Println("###### Welcome to our TodoList App! ######")
@@ -41,18 +31,5 @@ func main() {
 	http.HandleFunc("/hello-go", helloUser)
 	http.HandleFunc("/show-tasks", showTasks)
 	http.ListenAndServe(":8080", nil)
-
-	fmt.Println("Do you wish to update TodoList (Y/N)")
-	var answer string
-	fmt.Scan(&answer)
-	if answer == "Y" {
-		var newTask string
-		fmt.Println("Add task:")
-		fmt.Scan(&newTask)
-		taskItems = addTask(taskItems, newTask)
-		showTask(taskItems)
-	} else {
-		showTask(taskItems)
-	}
 
 }
