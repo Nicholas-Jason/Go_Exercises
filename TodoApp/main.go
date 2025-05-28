cb071b9bcef19b19ca3f4f4978b74f902f99a4ab
Nicholas-Jason Roache <njrr325@gmail.com>
2025-05-28 15:45:43 +0000

    removed unused code

--- TodoApp/main.go
+++ TodoApp/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
-	"os"
-	"strings"
 )
 
 var taskOne = "Practice Go"
@@ -17,7 +14,9 @@ var taskFive = "Go job hunting"
 var taskItems = []string{taskOne, taskTwo, taskThree, taskFour, taskFive}
 var user = "Jackson Richards"
 
-//add more functions
+type Post struct {
+	Task string `json: task`
+}
 
 func home(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Welcome to our TodoList App!")
@@ -35,35 +34,11 @@ func showTasks(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
-func addTask(writer http.ResponseWriter, request *http.Request) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Fprintf(writer, "Enter Task you wish to ask")
-
-	task, _ := reader.ReadString('\n')
-
-	task = strings.TrimSpace(task)
-	taskItems = append(taskItems, task)
-}
-func removeTask(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Enter Task you wish to remove")
-
-	var rtask string
-	fmt.Scanln(&rtask)
-	var new_taskItems []string
-	for _, task := range taskItems {
-		if task != rtask {
-			new_taskItems = append(new_taskItems, task)
-		}
-	}
-	taskItems = new_taskItems
-}
 func main() {
 
 	http.HandleFunc("/", home)
 	http.HandleFunc("/hello-go", helloUser)
-	http.HandleFunc("/add-tasks", addTask)
 	http.HandleFunc("/show-tasks", showTasks)
-	http.HandleFunc("/remove-tasks", removeTask)
 	http.ListenAndServe(":8080", nil)
 
 }
